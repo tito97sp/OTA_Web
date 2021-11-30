cd94c4db1f320398fa14f359c72d805f339c31cc
asanchez <tito97_sp@hotmail.com>
2021-11-30 01:38:53 +0100

    add Synamic Connection Setting

--- OTAWebApp/Startup.cs
+++ OTAWebApp/Startup.cs
@@ -27,11 +27,27 @@ namespace OTAWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
 
+            var DockerEnv = Environment.GetEnvironmentVariable("DOCKER");
+            if (DockerEnv != null)
+            {
+                if (DockerEnv == "true")
+                {
+                    services.AddDbContext<ApplicationDbContext>(options =>
+                        options.UseSqlServer(
+                            Configuration.GetConnectionString("DockerConnection")));
+
+                }
+            }
+            else 
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                        options.UseSqlServer(
+                            Configuration.GetConnectionString("DefaultConnection")));
+            }
+            
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
