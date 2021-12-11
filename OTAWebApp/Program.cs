c9a99829066f1c80012d313d4eaecb90ee20ac38
Andres Sanchez <tito97_sp@hotmail.com>
2021-12-11 01:04:57 +0100

    add migrations

--- OTAWebApp/Program.cs
+++ OTAWebApp/Program.cs
@@ -2,10 +2,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using OTAWebApp.Data;
+
+
 
 namespace OTAWebApp
 {
@@ -13,7 +18,17 @@ namespace OTAWebApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            
+            Console.WriteLine("Applying migrations");
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.Migrate();
+            }
+            Console.WriteLine("Done");
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
